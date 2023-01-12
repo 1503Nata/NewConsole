0103a7f76cba581700b40abedc5dd1fc1b3fdcd7
Natalya <pauls88@yandex.ru>
2023-01-12 23:48:12 +0500

    AddNewFile3Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 6;
-int numberB = 45;
-int result = numberA+numberB;
-Console.WriteLine(result);
+﻿int numberA = new Random ().Next(1, 10); // 1, 2, 3,...9
+Console.WriteLine(numberA);
+int numberB = new Random ().Next(1, 17); // 1, 2, 3,...16
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
