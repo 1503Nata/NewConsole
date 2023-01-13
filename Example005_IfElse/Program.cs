0016817719db48e90eac435585af45bfa52e7ac4
Natalya <pauls88@yandex.ru>
2023-01-13 13:02:08 +0500

    CorrectErrorExample005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "наталья")
+{
+  Console.WriteLine("Ура, это же Наташа!");
+}
+else
+{ 
+    Console.Write("Привет,  ");
+    Console.Write(username);
+}
