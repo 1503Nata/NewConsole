48c41cd24c9feb76aa7ef31219511481396a9e36
Natalya <pauls88@yandex.ru>
2023-01-13 20:26:49 +0500

    Bingo

--- Example008_BoyDog/Program.cs
+++ Example008_BoyDog/Program.cs
@@ -3,22 +3,25 @@ int firstfrendspeed = 1;
 int secondfrendspeed = 2;
 int dogspeed = 5;
 int frend = 2;
+int time = 0;
 
 int count = 0; 
 
-while (distans > 10)
-  {
-    if (frend == 1)
-    time = distans/(firstfrendspeed + dogspeed);
-    frend = 2;
-    count = count+1;
-  }
-  {
-    if (frend == 2)
-    time = distans/(secondfrendspeed - dogspeed);
-    frend = 1;
-    count = count+1;
-  }
+while (distans > 1000)
+{
+      if (frend == 1)
+     { time = distans/(firstfrendspeed + dogspeed);
+      distans = distans - (firstfrendspeed + secondfrendspeed)*time;
+      frend = 2;
+      }      
+  
+      if (frend == 2)
+     {  time = distans/(secondfrendspeed + dogspeed);
+      distans = distans - (firstfrendspeed + secondfrendspeed)*time;
+      frend = 1;
+      }
+count = count+1;
+}
 
-  Console.Write("Coбака пробежала", count, "раз");
-  Console.WriteLine(count);
+Console.Write("Coбака пробежала ");
+Console.WriteLine( count );
