ebf74088f4091776c5401494e5ad6b115632b966
Natalya <pauls88@yandex.ru>
2023-01-13 18:40:13 +0500

    correctfileexample008

---
+++ Example008_BoyDog/Program.cs
@@ -0,0 +1,24 @@
+﻿int distans = 10000;
+int firstfrendspeed = 1;
+int secondfrendspeed = 2;
+int dogspeed = 5;
+int frend = 2;
+
+int count = 0; 
+
+while (distans > 10)
+  {
+    if (frend == 1)
+    time = distans/(firstfrendspeed + dogspeed);
+    frend = 2;
+    count = count+1;
+  }
+  {
+    if (frend == 2)
+    time = distans/(secondfrendspeed - dogspeed);
+    frend = 1;
+    count = count+1;
+  }
+
+  Console.Write("Coбака пробежала", count, "раз");
+  Console.WriteLine(count);
