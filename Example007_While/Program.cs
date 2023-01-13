73c41b0e6b98eb9ad2068ffb30d94ca460ad081f
Natalya <pauls88@yandex.ru>
2023-01-13 14:02:59 +0500

    AddNewFile007

---
+++ Example007_While/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPosition (12, 4);
+//Console.WriteLine ("*");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 45;
+int xc = 40;
+int yc = 45;
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine ("*");
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine ("*");
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine ("*");
